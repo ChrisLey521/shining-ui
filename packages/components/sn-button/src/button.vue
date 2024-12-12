ebbf18bf9c6b35c193f73b7bfee1709f5d0023dd
Jeerrenn <461887214@qq.com>
2024-12-12 22:53:43 +0800

    feat: 配置eslint、stylelint

---
+++ packages/components/sn-button/src/button.vue
@@ -0,0 +1,37 @@
+<template>
+    <button :class="round ? 'round-button' : ''">
+
+    </button>
+</template>
+
+<script lang="ts" setup>
+import { ButtonSize, ButtonType, ButtonNativeType } from './type.ts'
+
+interface ButtonProps {
+    size?: ButtonSize
+    type?: ButtonType
+    plain?: boolean
+    text?: boolean
+    bg?: boolean
+    link?: boolean
+    round?: boolean
+    circle?: boolean
+    loading?: boolean
+    loadingIcon?: string
+    disabled?: boolean
+    icon?: string
+    autofocus?: boolean
+    nativeType?: ButtonNativeType
+    autoInsertSpace?: boolean
+    color?: string
+    dark?: boolean
+    tag?: string
+}
+defineProps<ButtonProps>()
+</script>
+
+<style scoped>
+.round-button {
+    border-radius: 10px;
+}
+</style>
---
+++ packages/components/sn-button/src/type.ts
@@ -0,0 +1,25 @@
+
+enum ButtonSize {
+    Mini = 'mini',
+    Small = 'small',
+    Medium = 'medium',
+    Large = 'large',
+}
+enum ButtonType {
+    Primary = 'primary',
+    Success = 'success',
+    Warning = 'warning',
+    Danger = 'danger',
+    Info = 'info',
+}
+
+enum ButtonNativeType {
+    Button = 'button',
+    Submit = 'submit',
+    Reset = 'reset',
+}
+export {
+    ButtonSize,
+    ButtonType,
+    ButtonNativeType
+}
